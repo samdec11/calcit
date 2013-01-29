7810f638ef9c7de449e37b1c2b53e93f986f75e4
Sean Marzug-McCarthy <samdec11@gmail.com>
2013-01-29 17:23:07 -0500

    added colors

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+require 'rainbow'
+
 def power(base, exponent)
   base ** exponent
 end
@@ -27,34 +29,34 @@ input = gets.chomp
 while input != "Q".downcase
   case input
   when "1"
-    print "Enter your first number. "
+    print "Enter your first number. ".color(:blue)
     number1 = gets.chomp.to_i
-    print "Enter your second number. "
+    print "Enter your second number. ".color(:blue)
     number2 = gets.chomp.to_i
-    print "What would you like to do with these numbers? Enter add, subtract, multiply, or divide. "
+    print "What would you like to do with these numbers? Enter add, subtract, multiply, or divide. ".color(:blue)
     enter = gets.chomp
     case enter
       when "add"
         x = addition(number1, number2)
-        puts "The sum of these numbers is #{x}."
+        puts "The sum of these numbers is #{x}.".color(:blue)
       when "subtract"
         x = subtraction(number1, number2)
-        puts "The difference of these numbers is #{x}."
+        puts "The difference of these numbers is #{x}.".color(:blue)
       when "multiply"
         x = multiplication(number1, number2)
-        puts "The product of these numbers is #{x}."
+        puts "The product of these numbers is #{x}.".color(:blue)
       when "divide"
         x = division(number1, number2)
-        puts "The quotient of these numbers is #{x}."
+        puts "The quotient of these numbers is #{x}.".color(:blue)
     end
   puts "What would you like to do next? Press 1 for basic calculations. Press 2 for advanced calculations. Press Q to quit. "
     input = gets.chomp
   when "2"
-    print "Enter your first number. "
+    print "Enter your first number. ".color(:red)
     number1 = gets.chomp.to_i
-    print "Enter your second number (enter 0 if performing square root function). "
+    print "Enter your second number (enter 0 if performing square root function). ".color(:red)
     number2 = gets.chomp.to_i
-    print "Would you like to raise a number to a power or find the square root of a number? Type power or root "
+    print "Would you like to raise a number to a power or find the square root of a number? Type power or root ".color(:red)
     enter = gets.chomp
     case enter
       when "power"
@@ -66,4 +68,4 @@ while input != "Q".downcase
   input = gets.chomp
   end
 end
-puts "You have quit the application."
+puts "You have quit the application.".color(:green)
