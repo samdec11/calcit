358a9d08d3459bc43bc1a253d496746682828a37
Sean Marzug-McCarthy <samdec11@gmail.com>
2013-01-29 15:58:21 -0500

    played with more code

--- main.rb
+++ main.rb
@@ -22,29 +22,35 @@ def division(number1, number2)
   number1/number2
 end
 
-puts "What would you like to do? Press 1 for basic calculations. Press 2 for advanced calculations. Press Q to quit."
+puts "What would you like to do? Press 1 for basic calculations. Press 2 for advanced calculations. Press Q to quit. "
 input = gets.chomp
 while input != "Q"
-case input
-when "1"
-  print "Enter your first number."
+  case input
+  when "1"
+  print "Enter your first number: "
   number1 = gets.chomp.to_i
-  print "Enter your second number."
+  print "Enter your second number: "
   number2 = gets.chomp.to_i
-  print "What would you like to do with these numbers? Enter add, subtract, multiply, or divide."
+  print "What would you like to do with these numbers? Enter add, subtract, multiply, or divide. "
+  enter = gets.chomp
   case enter
   when "add"
-    addition(number1, number2)
+    x = addition(number1, number2)
+    puts "The sum of these numbers is #{x}."
   when "subtract"
-    subtract(number1, number2)
+    x = subtract(number1, number2)
+    puts "The difference of these numbers is #{x}."
   when "multiply"
-    multiply(number1, number2)
+    x = multiply(number1, number2)
+    x
   when "divide"
-    division(number1, number2)
+    x = division(number1, number2)
+    x
   end
-puts "What would you like to do next? Press 1 for basic calculations. Press 2 for advanced calculations. Press Q to quit."
-when "2"
-  print "Would you like to raise a number to a power or find the square root of a number? Type power or root."
+  puts "What would you like to do next? Press 1 for basic calculations. Press 2 for advanced calculations. Press Q to quit."
+  when "2"
+    print "Would you like to raise a number to a power or find the square root of a number? Type power or root."
+  end
 end
 puts "You have quit the application."
 
