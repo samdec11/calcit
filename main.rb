5637ffb5421229871f87d21edc7a443893ee6281
Joseph Waine <joe.waine@gmail.com>
2013-01-29 15:10:52 -0500

    added basic calculator functions

--- main.rb
+++ main.rb
@@ -6,3 +6,19 @@ def square_root(number)
   number ** 0.5
 end
 
+def addition(number1, number2)
+  number1 + number2
+end
+
+def subtraction(number1, number2)
+  number1 - number2
+end
+
+def multiplication(number1, number2)
+  number1*number2
+end
+
+def division(number1, number2)
+  number1/number2
+end
+
