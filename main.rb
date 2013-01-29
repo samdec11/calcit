0ca6dfc384a69ee4716b044c7357b306def3d0d2
Sean Marzug-McCarthy <samdec11@gmail.com>
2013-01-29 15:19:09 -0500

    started loop

--- main.rb
+++ main.rb
@@ -1,3 +1,14 @@
+puts "What would you like to do? Press 1 for basic calculations. Press 2 for advanced calculations. Press Q to quit."
+input = gets.chomp
+
+case
+when input = "1"
+  print "Would you like to add, subtract, multiply, or divide?"
+when input = "2"
+  print "Would you like to "
+when input = "Q"
+end
+
 def power(base, exponent)
   base ** exponent
 end
