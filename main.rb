f644a018182eff65f46ce6f819a3a9b5622faeae
Sean Marzug-McCarthy <samdec11@gmail.com>
2013-01-29 15:39:50 -0500

    deleted redundant code

--- main.rb
+++ main.rb
@@ -48,27 +48,3 @@ when "2"
 end
 puts "You have quit the application."
 
-def power(base, exponent)
-  base ** exponent
-end
-
-def square_root(number)
-  number ** 0.5
-end
-
-def addition(number1, number2)
-  number1 + number2
-end
-
-def subtraction(number1, number2)
-  number1 - number2
-end
-
-def multiplication(number1, number2)
-  number1*number2
-end
-
-def division(number1, number2)
-  number1/number2
-end
-
